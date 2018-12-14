e0d94a6ef4fa4ed6ac4552f7c8553611c6f37b8b
CarlaCmpn <compan.carla@gmail.com>
2018-12-14 22:05:36 +0100

    Basic structure

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
+    sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Begin the experience', 'Presentation', 'Explication', 'Learn', 'Explore'],
+});
