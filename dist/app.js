798e5e9c9cba40c59dbeede808ea3ab17cb70872
CarlaCmpn <compan.carla@gmail.com>
2018-12-15 16:17:44 +0100

    Section1

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
-var myFullpage = new fullpage('#fullpage', {
-    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
-    // sectionsColor: ['#183252', '#183252', '#183252'],
-    navigation: true,
-    navigationPosition: 'right',
-});
+// var myFullpage = new fullpage('#fullpage', {
+//     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
+//     // sectionsColor: ['#183252', '#183252', '#183252'],
+//     navigation: true,
+//     navigationPosition: 'right',
+// });
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
 /**
  * 
  * 
+ *  FULLPAGE.JS
+ * 
+ *
+**/
+
+
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
+    navigation: true,
+    // menu: '#menu',
+});
+
+
+/**
+ * 
+ * 
  *  PARALLAX SECTION
  * 
  *
@@ -77,3 +93,62 @@ window.addEventListener('mousemove', (_event) =>
 /**
  * BUTTON START THE EXPERIENCE 
  */
+
+//  const button_begin = document.querySelector('.start')
+//  const section_1 = document.querySelector('#section1')
+
+//  button_begin.addEventListener('click', () =>
+// {
+
+// }
+
+/**
+ * DATE ET HEURE
+ */
+
+const T = () =>
+{
+    const $time_write = document.querySelector('.time')
+
+    const time = new Date()
+
+    const $hours = time.getHours()
+    const $minutes = time.getMinutes()
+    const $seconds = time.getSeconds()
+
+    $time_write.textContent = `${$hours} : ${$minutes} : ${$seconds}`
+
+    // if($minutes < 10)
+    // {
+    //     $minutes = "0" + $minutes;
+    // }
+
+    // if($seconds < 10)
+    // {
+    //     $seconds = "0" + $seconds;
+    // } 
+}
+
+setInterval('T()',1000);
+
+
+const D = () =>
+{
+    const $date_write = document.querySelector('.date')
+
+    const date = new Date()
+
+   
+    let days_name = new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
+    let months = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+    
+
+    const $days = days_name[date.getDay()] + " ";   
+    const $nbr = date.getDate() + " ";   
+    const $month = months[date.getMonth()] + '';
+    const $years = date.getFullYear()
+
+    $date_write.textContent = `${$days} ${$nbr} ${$month} ${$years}`
+}
+
+D()
