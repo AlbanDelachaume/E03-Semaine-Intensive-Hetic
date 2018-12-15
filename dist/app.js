ff11226ff90c1ea3f9803b4885be3f143f79f97f
CarlaCmpn <compan.carla@gmail.com>
2018-12-15 18:30:11 +0100

    Responsive Section 1

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-// var myFullpage = new fullpage('#fullpage', {
-//     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
-//     // sectionsColor: ['#183252', '#183252', '#183252'],
-//     navigation: true,
-//     navigationPosition: 'right',
-// });
