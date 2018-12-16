8bedf104c657538404504c10e113be8a22dff81d
CarlaCmpn <32802792+CarlaCmpn@users.noreply.github.com>
2018-12-16 20:18:20 +0100

    Add files via upload

---
+++ E03-Semaine-Intensive-Hetic/scripts/script.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 
+ *  FULLPAGE.JS
+ * 
+ *
+**/
+
+
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourth', 'fifthPage'],
+    navigation: true,
+    // menu: '#menu',
+});
